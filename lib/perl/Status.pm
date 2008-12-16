4a1c26af0845772affb37cf91646abc5760edfb8
Mark Norman Francis <norm@cackhanded.net>
2008-12-16 03:30:26 +0000

    perl library: Move the pad_text function out of Status and into a dedicated Text::Trim package.

--- lib/perl/Status.pm
+++ lib/perl/Status.pm
@@ -6,12 +6,14 @@ use warnings;
 use IO::Interactive     qw( is_interactive );
 use Readonly;
 use Term::ANSIColor;
+use Text::Trim;
+
 
 use Exporter;
 our @ISA    = qw( Exporter );
 our @EXPORT = qw( status  status_line  print_line );
 
-Readonly my $MAX_LINE_LENGTH => 77;     # 80 -indent -right-margin
+Readonly my $MAX_LINE_LENGTH => 79;     # 80 - 1 for right-margin
 
 
 # make sure output is sent immediately
@@ -19,47 +21,24 @@ $| = 1;
 
 
 sub status {
-    my $text   = pad_text( shift );
+    my $text   = pad_text( shift, $MAX_LINE_LENGTH );
     my $colour = shift || q();
 
     print colored( "$text\r", $colour ) if is_interactive();
 }
 sub status_line {
-    my $text   = pad_text( shift );
+    my $text   = pad_text( shift, $MAX_LINE_LENGTH );
     my $colour = shift || q();
     
     print is_interactive() ? colored( "$text\n", $colour )
                            : "$text\n";
 }
 sub print_line {
-    my $text   = pad_text( shift, 1 );
+    my $text   = pad_text( shift, $MAX_LINE_LENGTH, 'no-trim' );
     my $colour = shift || q();
 
     print is_interactive() ? colored( "$text\n", $colour )
                            : "$text\n";
-}
-
-
-sub pad_text {
-    my $text    = shift;
-    my $no_left = shift;
-    
-    chomp $text;
-    my $len = length($text);
-
-    # replace the text in the middle with ... if it is wider than the max
-    if ( $MAX_LINE_LENGTH < $len ) {
-        my $over   = ( $len+3 ) - $MAX_LINE_LENGTH;
-        my $mid    = int( $len  / 2 );
-        my $strip  = int( $over / 2 );
-        my $offset = $mid - $strip;
-    
-        substr( $text, $offset, $over, '...' );
-    }
-    
-    my $left  = defined $no_left ? q() : q(  );
-    my $right = q{ } x ( $MAX_LINE_LENGTH - $len );
-    return $left . $text . $right;
 }
 
 1;
