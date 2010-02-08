dda5e1da5fb4d45ba2013c52bb318aa3450469c3
Mark Norman Francis <norm@cackhanded.net>
2010-02-08 14:30:08 +0000

    Add base of scripts to control window placement when switching between laptop-only and laptop-with-external-monitor modes.

---
+++ Library/Scripts/Set up for dual screens.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+osascript <<APPLESCRIPT
+    set_spaces_binding( "com.apple.mail", 65544 )
+    resize_application( "Mail", { 1920, 22, 2854, 1200 } )
+    
+    
+    
+    to resize_application ( app_name, size )
+        tell application app_name to ¬
+            set the bounds of the first window to size
+    end resize_application
+    
+    to set_spaces_binding ( identifier, binding ) 
+        set new_binding to ¬
+            (run script "{|" & identifier & "|:" & binding & "}")
+        tell application "System Events"
+            tell expose preferences
+                tell spaces preferences
+                    set spaces_bindings to get application bindings
+                    set application bindings ¬
+                        to new_binding & spaces_bindings
+                end tell
+            end tell
+        end tell
+    end set_spaces_binding
+APPLESCRIPT
