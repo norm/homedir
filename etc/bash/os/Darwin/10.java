ae83fae9820b88088625f67e69560039b5d60c1f
Mark Norman Francis <norm@cackhanded.net>
2010-05-08 12:18:46 +0100

    'home' migration: Update Darwin (OS X) settings.

---
+++ etc/bash/os/Darwin/10.java
@@ -0,0 +1,6 @@
+# github:etc/bash/os/Darwin/10.java
+# -*- Mode: Bash; tab-width: 4; indent-tabs-mode: nil; -*-
+#
+#       Java comes for free with OS X.
+
+export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
