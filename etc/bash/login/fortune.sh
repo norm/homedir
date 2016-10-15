a8ce63c966cc061ff42aa0e1ff1e445b22371b6a
Mark Norman Francis <norm@cackhanded.net>
2016-10-15 23:53:18 +0100

    fixup fortune

--- etc/bash/login/fortune.sh
+++ etc/bash/login/fortune.sh
@@ -1,4 +1,8 @@
-# I am a nerd.
-echo ''
-available fortune \
-    && fortune startrek
+# github:norm/homedir:etc/bash/login/fortune.sh
+# 
+# Yes, I am a nerd.
+
+available fortune && {
+    echo ''
+    fortune startrek
+}
