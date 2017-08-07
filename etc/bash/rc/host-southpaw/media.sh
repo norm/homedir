34668f05d77aa1526d5b564e256e2164534edea2
Mark Norman Francis <norm@201created.com>
2017-08-07 15:11:52 +0100

    Configure media and add TV shortcut

---
+++ etc/bash/rc/host-southpaw/media.sh
@@ -0,0 +1,22 @@
+# github:norm/homedir:etc/bash/rc/host-southpaw/media.sh
+#
+# Settings for dealing with media, especially anything to do with
+# https://github.com/norm/media-tools.
+
+export MEDIA_CONFIG="${HOME}/etc/media.conf"
+
+
+function tv {
+    local show="${1//\//}";
+    local season=$(
+        /bin/ls "/files/tv/${show}/" \
+            | awk '/Season / { print $2 }' \
+            | sort -rn \
+            | head -1
+    )
+
+    print "${bold}${red}${show} Season ${season}:${reset}"
+    ls "/files/tv/${show}/Season ${season}"
+}
+
+complete -o filenames -F _tv_series tv
