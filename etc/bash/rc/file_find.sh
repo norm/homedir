134e05f401c59cce9045a03fb669d81cf8aaa2c7
Mark Norman Francis <norm@201created.com>
2022-01-31 14:31:18 +0000

    Prefer ripgrep for finding in files

--- etc/bash/rc/file_find.sh
+++ etc/bash/rc/file_find.sh
@@ -8,8 +8,16 @@ alias fgr='find . | egrep -i'
 alias ffgr='find . -type f | egrep -i'
 alias fgx='find . -type f -print0 | xargs -0 egrep -i'
 
+# prefer ack over grep
 available ack \
     && alias fgx='ack -ai'
 
+# prefer the_silver_searcher over ack
 available ag \
     && alias fgx='ag -ai'
+
+# prefer ripgrep over the_silver_searcher
+available rg && {
+    alias fgx='rg --smart-case --no-ignore --hidden'
+    alias rg='rg --smart-case'
+}
